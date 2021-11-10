cce165568188526fe030792cab1af1f4f3a1ca1c
Sai <sai@ptmba92.local>
2021-11-10 21:31:32 +0530

    template driven form validations UT

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -21,42 +21,103 @@ describe('AppComponent', () => {
     componentInstance = fixture.componentInstance;
   });
 
-  it('check the form values in the form with ngmodel controller values', () => {
+  it('username and password validation for required field', () => {
     fixture.detectChanges();
     fixture.whenStable().then(() => {
+      const usernameElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#username');
+      const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#password');
+      const genderElement: HTMLSelectElement = fixture.debugElement.nativeElement.querySelector('#gender');
 
-      const loginSubmitMock = spyOn(componentInstance, 'loginSubmit').and.callThrough();
-      const usernameElement:HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#usernameId');
-      usernameElement.value = 'usernameSample';
-      usernameElement.dispatchEvent(new Event('input'));
+      usernameElement.dispatchEvent(new Event('foucs'));
+      usernameElement.dispatchEvent(new Event('blur'));
 
-      const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#passwordId');
-      passwordElement.value = 'passwordsample';
-      passwordElement.dispatchEvent(new Event('input'));
+      passwordElement.dispatchEvent(new Event('focus'));
+      passwordElement.dispatchEvent(new Event('blur'));
 
-      const submitBtnElement: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#buttonId');
-      submitBtnElement.click();
+      genderElement.dispatchEvent(new Event('focus'));
+      genderElement.dispatchEvent(new Event('blur'));
+
       fixture.detectChanges();
       fixture.whenStable().then(() => {
-        expect(componentInstance.loginFormObj.username).toEqual('usernameSample');
-        expect(componentInstance.loginFormObj.password).toEqual('passwordsample');
-        expect(loginSubmitMock).toHaveBeenCalled();
+        const usernameErrors: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#usernameErrors');
+        const passwordErrors: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#passwordErrors');
+        const genderErrors: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#genderErrors');
+        const submitButton: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#submitButton');
+
+        expect(usernameErrors.children.length).toEqual(1);
+        expect(usernameErrors.children[0].innerHTML).toEqual('This field required');
+
+        expect(passwordErrors.children.length).toEqual(1);
+        expect(passwordErrors.children[0].innerHTML).toEqual('This field required');
+
+        expect(genderErrors.children.length).toEqual(1);
+        expect(genderErrors.children[0].innerHTML).toEqual('This field required');
+
+        expect(submitButton.disabled).toBeTruthy();
       })
     })
   })
 
-  it('update login object in the controller and check html element content', () => {
+  it('username and password validation with patterns', () => {
     fixture.detectChanges();
     fixture.whenStable().then(() => {
-      componentInstance.loginFormObj.username = 'sai kumar';
-      componentInstance.loginFormObj.password = 'sai@1234';
+      const usernameElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#username');
+      const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#password');
+      
+      usernameElement.value = 'sai@gmail';
+      passwordElement.value = 'sai&';
+
+      usernameElement.dispatchEvent(new Event('input'));
+      passwordElement.dispatchEvent(new Event('input'));
+
       fixture.detectChanges();
       fixture.whenStable().then(() => {
-        const usernameElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#usernameId');
-        const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#passwordId');
+        const usernameErrors: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#usernameErrors');
+        const passwordErrors: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#passwordErrors');
+        const submitButton: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#submitButton');
 
-        expect(usernameElement.value).toEqual('sai kumar');
-        expect(passwordElement.value).toEqual('sai@1234');
+        expect(usernameErrors.children.length).toEqual(1);
+        expect(usernameErrors.children[0].innerHTML).toEqual('Please enter a valid email pattern');
+
+        expect(passwordErrors.children.length).toEqual(1);
+        expect(passwordErrors.children[0].innerHTML).toEqual('This field should have min of 5 char and 1 alphabet, 1 numeric and atleast 1 special char');
+      
+        expect(submitButton.disabled).toBeTruthy();
+      })
+    })
+  })
+
+  it('check validity for entire proper data ', () => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const usernameElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#username');
+      const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#password');
+      const genderElement: HTMLSelectElement = fixture.debugElement.nativeElement.querySelector('#gender');
+
+      usernameElement.value = 'sai@gmail.com';
+      passwordElement.value = 'sai@123';
+      genderElement.value = 'female';
+
+      usernameElement.dispatchEvent(new Event('input'));
+      passwordElement.dispatchEvent(new Event('input'));
+      genderElement.dispatchEvent(new Event('change'));
+
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        const usernameErrors: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#usernameErrors');
+        const passwordErrors: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#passwordErrors');
+        const genderErrors: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#genderErrors');
+        const submitButton: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#submitButton');
+
+        expect(usernameErrors).toBeNull();
+        expect(passwordErrors).toBeNull();
+        expect(genderErrors).toBeNull();
+
+        expect(submitButton.disabled).toBeFalsy();
+
+        expect(componentInstance.registerObj.username).toEqual('sai@gmail.com');
+        expect(componentInstance.registerObj.password).toEqual('sai@123');
+        expect(componentInstance.registerObj.gender).toEqual('female');
       })
     })
   })
