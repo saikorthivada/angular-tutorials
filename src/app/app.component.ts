5bdaeac4fcd097f538f43263319445abb397fbc2
Sai <sai@ptmba92.local>
2021-11-14 14:47:26 +0530

    Form control validations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,5 +7,16 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  username: FormControl= new FormControl('');
+
+  email: FormControl = new FormControl('', [Validators.required, Validators.pattern(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]);
+
+  password: FormControl = new FormControl('',[Validators.required, Validators.pattern(/(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]{5,10}/)]);
+
+  login() {
+    console.log(this.email, '*****', this.password);
+    return {
+      email: this.email,
+      password: this.password
+    }
+  }
 }
