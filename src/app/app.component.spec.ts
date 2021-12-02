afc8ccfec48ffa7d0f507f661dbf5f51753c812a
Sai <sai@ptmba92.local>
2021-12-02 23:54:30 +0530

    Unit testing set value method of form control

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -7,6 +7,7 @@ describe('AppComponent', () => {
 
   let fixture: ComponentFixture<AppComponent>;
   let componentInstance: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -21,95 +22,23 @@ describe('AppComponent', () => {
     componentInstance = fixture.componentInstance;
   });
 
-  it('check the validation for required in password and email', () => {
+  it('check the input value change which should change the form control', () => {
     fixture.detectChanges();
     fixture.whenStable().then(() => {
-      const emailElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#emailElement');
-      emailElement.value = '';
-      emailElement.dispatchEvent(new Event('input'));
-
-      const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#passwordElement');
-      passwordElement.value = '';
-      passwordElement.dispatchEvent(new Event('input'));
+      const usernameElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#username');
+      usernameElement.value = 'sai@gmail.com';
+      usernameElement.dispatchEvent(new Event('input'));
+      expect(componentInstance.username.value).toEqual('sai@gmail.com');
+      const button: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#buttonID');
+      button.click();
+      expect(componentInstance.username.value).toEqual('dynamic value');
 
       fixture.detectChanges();
       fixture.whenStable().then(() => {
-        const emailValidator: HTMLParagraphElement = fixture.debugElement.nativeElement.querySelector('#emailRequired');
-        expect(emailValidator).not.toBeNull();
-        expect(emailValidator.innerHTML).toEqual('Email is required');
-
-        const passwordValidators: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#passwordValidations');
-        expect(passwordValidators.children.length).toEqual(1);
-        expect(passwordValidators.children[0].innerHTML).toEqual('Password is required');
-
-        const loginButton: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#loginButton');
-        const loginMock = spyOn(componentInstance, 'login');
-        expect(loginButton.disabled).toBeTruthy();
-        loginButton.click();
-        expect(loginMock).toHaveBeenCalledTimes(0);
+        const updatedUsername: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#username');
+        expect(updatedUsername.value).toEqual('dynamic value');
       })
     })
   })
 
-  it('check the validation for pattern in password and email field', () => {
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      const emailElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#emailElement');
-      emailElement.value = 'sai@gmail';
-      emailElement.dispatchEvent(new Event('input'));
-
-      const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#passwordElement');
-      passwordElement.value = 'sai12';
-      passwordElement.dispatchEvent(new Event('input'));
-
-      fixture.detectChanges();
-      fixture.whenStable().then(() => {
-        const emailPatternValidator: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#emailPattern');
-        expect(emailPatternValidator).not.toBeNull();
-        expect(emailPatternValidator.innerHTML).toEqual('Enter a valid email pattern');
-
-        const passwordPatterns: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#passwordValidations');
-        expect(passwordPatterns.children.length).toEqual(1);
-        expect(passwordPatterns.children[0].innerHTML).toEqual('Enter a password with min of 5 char with atleast 1 alphabet 1 numeric value and one special char');
-
-        const loginButton: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#loginButton');
-        expect(loginButton.disabled).toBeTruthy();
-        const loginMock = spyOn(componentInstance, 'login');
-        loginButton.click();
-        expect(loginMock).toHaveBeenCalledTimes(0);
-      })
-    })
-  })
-
-  it('check when the values are satisfying the validations', () => {
-    fixture.detectChanges();
-    fixture.whenStable().then(() => {
-      const emailElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#emailElement');
-      emailElement.value = 'sai@gmail.com';
-      emailElement.dispatchEvent(new Event('input'));
-
-      const passwordElement: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#passwordElement');
-      passwordElement.value = 'sai@123';
-      passwordElement.dispatchEvent(new Event('input'));
-
-      fixture.detectChanges();
-      fixture.whenStable().then(() => {
-
-        const emailRequiredValidator: HTMLParagraphElement = fixture.debugElement.nativeElement.querySelector('#emailRequired');
-        expect(emailRequiredValidator).toBeNull();
-        
-        const emailPatternValidator: HTMLParagraphElement = fixture.debugElement.nativeElement.querySelector('#emailPattern');
-        expect(emailPatternValidator).toBeNull();
-
-        const passwordPatterns: HTMLDivElement = fixture.debugElement.nativeElement.querySelector('#passwordValidations');
-        expect(passwordPatterns).toBeNull();
-
-        const loginButton: HTMLButtonElement = fixture.debugElement.nativeElement.querySelector('#loginButton');
-        expect(loginButton.disabled).toBeFalsy();
-        const loginMock = spyOn(componentInstance, 'login');
-        loginButton.click();
-        expect(loginMock).toHaveBeenCalledTimes(1);
-      })
-    })
-  })
 });
