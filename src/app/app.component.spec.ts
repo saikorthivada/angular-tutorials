4adea7a7b2155e2390ffd5c5690f6304294b6823
Sai kumar <korthivadasaikumar@gmail.com>
2022-02-04 22:51:38 +0530

    Form group testing with form controls

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,15 +4,41 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-
+  let fixture: ComponentFixture<AppComponent>;
+  let componentInstance: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        ReactiveFormsModule
       ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    componentInstance = fixture.componentInstance;
   });
+
+  it('check the form controls inside a form group', () => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const email: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#email');
+      const password: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#password');
+
+      email.value = 'saikumar',
+      password.value = '123456',
+
+      email.dispatchEvent(new Event('input'));
+      password.dispatchEvent(new Event('input'));
+
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+        expect(componentInstance.loginFormGroup.value).toEqual({
+          email: 'saikumar',
+          passwordReset: '123456'
+        })
+      })
+    })
+  })
 });
