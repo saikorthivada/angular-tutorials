99da7b241f60cefed267d6df996019e60f6557f4
Sai kumar <korthivadasaikumar@gmail.com>
2022-03-15 20:51:39 +0530

    setvalue method in reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,25 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
-
+import { FormControl, FormGroup } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  resetFornGroup: FormGroup;
 
-  loginFormGroup: FormGroup;
+  defaultFormValues = {
+    username: 'sai@gmail.com',
+    password: 'passwordupdate'
+  }
   constructor() {
-    this.loginFormGroup = new FormGroup({
-      username: new FormControl('', Validators.compose([Validators.required])),
-      password: new FormControl('', Validators.compose([Validators.required]))
+    this.resetFornGroup = new FormGroup({
+      username: new FormControl(''),
+      password: new FormControl('')
     })
   }
 
-  login() {
-    console.log(this.loginFormGroup.value);
-    console.log(this.loginFormGroup.valid);
-  }
-
-  getFormControl(formControlName: string) {
-    return this.loginFormGroup.get(formControlName);
+  resetForm() {
+    this.resetFornGroup.setValue(this.defaultFormValues);
   }
 }
