ac67b712baa936564858220c52609ecd6371c208
FIRST_NAME LAST_NAME <korthivadasaikumar@gmail.com>
2022-05-31 07:48:43 +0530

    added address management system uts

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,9 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let componentInstance: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -14,6 +16,20 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    componentInstance = fixture.componentInstance;
   });
+
+  it('value with default object check', () => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const elements: HTMLInputElement[] = fixture.debugElement.nativeElement.querySelectorAll('input');
+      expect(elements.length).toEqual(10);
+      expect(JSON.stringify(componentInstance.obj)).toEqual(JSON.stringify(componentInstance.registerAddress.value));
+
+      expect(componentInstance.addressesAsFormArray.length).toEqual(componentInstance.obj.addresses.length);
+      expect(JSON.stringify(componentInstance.addressesAsFormArray.value)).toEqual(JSON.stringify(componentInstance.obj.addresses));
+    })
+  })
 
 });
