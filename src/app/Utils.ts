37c95688218024d3a89d85ad38ab3c8e16a10c8a
FIRST_NAME LAST_NAME <korthivadasaikumar@gmail.com>
2022-07-02 20:34:20 +0530

    Interfaces in angular

---
+++ src/app/Utils.ts
@@ -0,0 +1,4 @@
+export interface IUtils {
+    name: string;
+    age: number;
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { IService } from './Service';
+import { IUtils } from './Utils';
 
+interface ICommon extends IUtils, IService {
+
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-
+export class AppComponent implements ICommon {
+  // name = 'sai kumar';
+  name = 'sai kumar';
+  age = 20;
 }
