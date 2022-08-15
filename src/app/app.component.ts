00cce0bcbafe75d50ff59908ba82085e0470cd3c
Sai kumar skk <korthivadasaikumar@gmail.com>
2022-08-15 12:52:39 +0530

    Parent to child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
+  parentTitle: string = 'Parent data updated';
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  templateUrl: './child.component.html',
+  styleUrls: ['./child.component.scss']
+})
+export class ChildComponent implements OnInit {
+
+  @Input()
+  title: string = 'Child component';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
