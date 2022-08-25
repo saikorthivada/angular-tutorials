31f53497de29512dd9215e4bad2a5a110964c4d0
Sai kumar skk <korthivadasaikumar@gmail.com>
2022-08-25 22:34:03 +0530

    Unit testing on changes life cycle

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,39 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './child/child.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let componentInstance: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        FormsModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ChildComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    componentInstance = fixture.componentInstance;
   });
+
+  it('Meters value passed from two way binding to ng changes', () =>{
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      const element: HTMLInputElement = fixture.debugElement.nativeElement.querySelector('#modelValue');
+      element.value = '10';
+      element.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      fixture.whenStable().then(() => {
+
+        const resultElement: HTMLParagraphElement = fixture.debugElement.nativeElement.querySelector('#result');
+        expect(resultElement.innerHTML).toEqual(`Meters into centimeters - 1000`);
+      })
+    })
+  })
 });
