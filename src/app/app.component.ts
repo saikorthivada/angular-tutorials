a432f0a65ac041f68fa986a70e9269fd68d2e871
Sai kumar skk <korthivadasaikumar@gmail.com>
2022-08-28 12:20:55 +0530

    View child on input elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('sample')  element!: ElementRef;
+  @ViewChild('changeStyles') paraelement!: ElementRef;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.element.nativeElement.focus();
+    this.paraelement.nativeElement.style.color = 'red';
+  }
+  
 }
