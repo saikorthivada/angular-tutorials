316f34d70a0062b1db21d1d7f31c0173ee8ab3f6
Sai kumar skk <korthivadasaikumar@gmail.com>
2022-08-29 16:16:06 +0530

    Implemented test cases for view child

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,10 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let componentInstance: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -12,5 +14,23 @@ describe('AppComponent', () => {
         AppComponent
       ],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    componentInstance = fixture.componentInstance;
   });
+
+  it('First input field should be focused, even when we have multiple same # varaibles', () => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() => {
+      expect(document.activeElement?.id).toEqual('firstElement');
+      expect(document.activeElement?.id).not.toEqual('secondElement');
+    })
+  });
+
+  it('On load the para should be in red color', () => {
+    fixture.detectChanges();
+    fixture.whenStable().then(() =>{
+      const para_element: HTMLParagraphElement = fixture.debugElement.nativeElement.querySelector('#para_element');
+      expect(para_element.style.color).toEqual('red');
+    })
+  })
 });
