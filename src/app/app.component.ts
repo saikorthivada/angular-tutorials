9128f071147149b8a82b1ebcb72b0733c5637869
Sai kumar skk <korthivadasaikumar@gmail.com>
2022-11-29 20:38:15 +0530

    Updated to v15

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,29 +7,4 @@ import { FormBuilder, FormGroup } from '@angular/forms';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  addressForm: FormGroup;
-
-  constructor(private formBuilder: FormBuilder) {
-    this.addressForm = this.formBuilder.group({
-      name: [''],
-      phone: [''],
-      address: this.getAddressFormGroup()
-    })
-  }
-
-  getAddressFormGroup(): FormGroup<any> {
-    return this.formBuilder.group({
-      streetName: [''],
-      roadNumber: ['']
-    }) as FormGroup<any>;
-  }
-
-  getAddressFormGroupForChild(): FormGroup<any> {
-    return this.addressForm.get('address') as FormGroup;
-  }
-
-  submit() {
-    console.log(this.addressForm.value);
-  }
 }
