8e6f8ec8e35b47b983d8b16920097c52db9bc7cd
Sai kumar skk <korthivadasaikumar@gmail.com>
2022-12-19 17:55:34 +0530

    Revert "Dependent dropdown using reactive forms"
    This reverts commit bb4f7967085f5c7d215bc4932f93a440ffbedf12.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,71 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { Component } from '@angular/core';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+
+const checkEquailty = (source: string, target: string) => {
+  return (group: AbstractControl): ValidationErrors | null => {
+    const sourceValue = group.get(source)?.value;
+    const targetValue = group.get(target)?.value;
+    if(sourceValue !== targetValue) {
+      return {
+        notEqual: true
+      }
+    }
+    return null;
+  }
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit {
+export class AppComponent {
 
-  list = [
-    {
-      id: 1,
-      label: 'Asia',
-      countries: [
-        {
-          id: 101,
-          label: 'India'
-        },
-        {
-          id: 102,
-          label: 'Iran'
-        }
-      ]
-    },
-    {
-      id: 2,
-      label: 'Europe',
-      countries: [
-        {
-          id: 201,
-          label: 'France'
-        },
-        {
-          id: 202,
-          label: 'Germany'
-        }
-      ]
-    }
-  ];
-
-  selectedContriesList: any[] = [];
-
-  dropDownForm!: FormGroup;
+  passwordGroup: FormGroup;
 
   constructor() {
-
-  }
-
-  ngOnInit(): void {
-    this.dropDownForm = new FormGroup({
-      contintent: new FormControl(null),
-      country: new FormControl({value: null, disabled: true})
-    });
-
-    this.dropDownForm.get('contintent')?.valueChanges.subscribe((res: number) => {
-      console.log(res);
-      this.dropDownForm.get('country')?.setValue(null);
-      if(res) {
-        this.selectedContriesList = this.list.filter((obj: any) => obj.id === res)[0].countries;
-        this.dropDownForm.get('country')?.enable();
-      } else {
-        this.dropDownForm.get('country')?.disable();
-      }
-    })
-  }
-
-  submit() {
-    console.log(this.dropDownForm.value);
+    this.passwordGroup = new FormGroup({
+      password: new FormControl(''),
+      confirmPassword: new FormControl('')
+    }, [checkEquailty('password', 'confirmPassword')])
+  
   }
 }
