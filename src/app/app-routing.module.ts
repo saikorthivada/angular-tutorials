7c604f272782e9b1cfa4b8ba98ffd92b78e65576
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-03-18 17:21:46 +0530

    Nested routes standalone approach

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,31 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LoginComponent } from './login/login.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/register',
+    redirectTo: '/products',
     pathMatch: 'full'
   },
   {
-    path: 'login',
-    component: LoginComponent
-  },
-  {
-    path: 'register',
-    component: RegisterComponent
-  },
-  {
-    path: 'dashboard',
-    component: DashboardComponent
-  },
-  {
-    path: '**',
-    component: NotfoundComponent
+    path: 'products',
+    loadChildren: () => import('./products/products-routes').then(routes => routes.ProductsRoute)
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { NotfoundComponent } from './notfound/notfound.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    NotfoundComponent,
-    DashboardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
