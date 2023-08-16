e674a1d13524df885ab076edc8bef399081c936f
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-08-16 14:59:43 +0530

    Injection process using inject method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { MessageService } from './message.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  messageService!: MessageService;
 
+  constructor() {
+    this.messageService = inject(MessageService);
+  }
 }
