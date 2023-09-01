097ba3e396fb82b6cb3abb3ed4dcf81b9c7e4a07
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-09-01 19:47:13 +0530

    Observables introduction

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  timer: any;
+  sampleObseravable = Observable.create((observer: any) => {
+    let count = 0;
+    this.timer = setInterval(() => {
+      console.log('timer rurning');
+      if (count <= 10) {
+        observer.next(count);
+        count++;
+      } else {
+        observer.complete();
+      }
+    }, 1000);
 
+  });
+
+  constructor() {
+    this.sampleObseravable.subscribe(
+      (result: any) => {
+        console.log('subscriber', result);
+      }
+    );
+
+    this.sampleObseravable.subscribe(
+      (result: any) => {
+        console.log('subscriber 2', result);
+      }
+    )
+  }
 }
