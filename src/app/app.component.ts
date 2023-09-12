e95a63f2bda250fd2e5c27cf6c40d46f49bacfe5
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-09-12 22:37:35 +0530

    Pipe and map in observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  sampleObservable: Observable<any> = of(1, 2, 3, 4);
 
+  httpClient: HttpClient = inject(HttpClient)
+
+  constructor() {
+    this.httpClient.get('https://jsonplaceholder.typicode.com/todos').pipe(
+      map((result: any) => {
+        return result?.map((obj: any)=>{
+          return {
+            id: obj.id,
+            title: obj.title
+          }
+        })
+      })
+    ).subscribe((todos: any) => {
+      console.log(todos);
+    })
+    // this.sampleObservable.pipe(
+    //   map((result) => {
+    //     return result * 2;
+    //   })
+    // ).subscribe((res) => {
+    //   console.log(res);
+    // })
+  }
 }
