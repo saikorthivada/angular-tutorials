7b3aa78a97d90a6e12a8be9ac2a2f8bb12163a24
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-09-23 11:37:36 +0530

    Filter and map in angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import {filter, map, tap} from 'rxjs/operators';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,18 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  // ofObservable: Observable<number> = of(1, 2, 3, 4);
+
+  http = inject(HttpClient)
+
+  constructor() {
+    this.http.get(`https://jsonplaceholder.typicode.com/todos`).pipe(
+      filter((list: any) => {
+        const index = list.findIndex((obj: any) => obj.id === -1);
+        return index !== -1;
+      }),
+    ).subscribe((res) => {
+      console.log('subscribe',res);
+    })
+  }
 }
