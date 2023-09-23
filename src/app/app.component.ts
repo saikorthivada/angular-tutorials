897617ece68533466a556a27c812210f944dc32c
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-09-23 11:56:56 +0530

    Switch merge and concat map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { of, Observable } from 'rxjs';
+import { concatMap, mergeMap, switchMap} from 'rxjs/operators'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  ofObservable: Observable<number> = of(1, 2, 3, 4);
 
+  http: HttpClient = inject(HttpClient);
+  constructor() {
+    this.ofObservable.pipe(
+      // mergeMap((value: number) => {
+      //   return this.http.get(`https://jsonplaceholder.typicode.com/todos/${value}`);
+      // })
+      // switchMap((value: number) => {
+      //   return this.http.get(`https://jsonplaceholder.typicode.com/todos/${value}`);
+      // })
+      concatMap((value: number) => {
+        return this.http.get(`https://jsonplaceholder.typicode.com/todos/${value}`);
+      })
+    ).subscribe((res) => {
+      console.log(res);
+    })
+  }
 }
