820cdb547e1562def2a3be5c82ebeda631ce621a
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-10-02 20:12:30 +0530

    Async pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,23 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  list!: Observable<any>;
+
+  show = of(true);
+
+  http = inject(HttpClient);
+
+  // uiList: number[] = [];
+
+  constructor() {
+    // this.list.subscribe((res: number[]) => {
+    //   console.log(res);
+    //   this.uiList = res;
+    // })
+    this.getList();
+  }
+
+  getList(): void {
+    this.list = this.http.get('https://jsonplaceholder.typicode.com/todos');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,6 +12,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
