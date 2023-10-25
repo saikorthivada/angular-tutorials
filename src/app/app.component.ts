32f1bef27a4abb4c93ce0a96debde5c017afa5cb
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-10-25 18:31:34 +0530

    Subject vs behaviour subject

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { BehaviorSubject, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  // subject = new Subject();
 
+  behaviourSubject = new BehaviorSubject('');
+
+  constructor() {
+
+     this.behaviourSubject.subscribe((res) => {
+      console.log('Subscriber one - ', res);
+    });
+
+    this.behaviourSubject.next('Emission 1');
+
+    this.behaviourSubject.subscribe((res) => {
+      console.log('Subscriber Two - ', res);
+    });
+    this.behaviourSubject.next('Emission 2');
+
+    // this.subject.subscribe((res) => {
+    //   console.log('Subscriber one - ', res);
+    // });
+
+    // this.subject.next('Emission 1');
+
+    // this.subject.subscribe((res) => {
+    //   console.log('Subscriber Two - ', res);
+    // });
+    // this.subject.next('Emission 2');
+  }
 }
