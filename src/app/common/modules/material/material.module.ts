732bf0706662334a20934707e2e1efde85512c9c
Sai kumar skk <korthivadasaikumar@gmail.com>
2023-12-15 18:36:22 +0530

    Notes list module

--- src/app/common/modules/material/material.module.ts
+++ src/app/common/modules/material/material.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatButtonModule} from '@angular/material/button';
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
-import {MatInputModule} from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatCheckboxModule} from '@angular/material/checkbox';
-import {MatSnackBarModule} from '@angular/material/snack-bar';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatDialogModule} from '@angular/material/dialog';
-
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { MatTableModule } from '@angular/material/table'
 @NgModule({
   declarations: [],
   imports: [
@@ -26,7 +28,10 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatSnackBarModule,
     MatToolbarModule,
     MatMenuModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
   ],
   exports: [
     MatSlideToggleModule,
@@ -39,7 +44,10 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatSnackBarModule,
     MatToolbarModule,
     MatMenuModule,
-    MatDialogModule
+    MatDialogModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule,
   ]
 })
 export class MaterialModule { }
--- src/app/views/admin-views/notes/notes.component.ts
+++ src/app/views/admin-views/notes/notes.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
+import { LOCALSTORAGE_KEYS } from 'src/app/common/constants/local-storage.constants';
+import { INote, NotesService } from 'src/app/services/notes.service';
 
 @Component({
   selector: 'app-notes',
   templateUrl: './notes.component.html',
   styleUrls: ['./notes.component.scss']
 })
-export class NotesComponent {
+export class NotesComponent implements OnInit {
+  displayedColumns: string[] = ['id', 'title', 'description', 'createdAt', 'updatedAt', 'Actions'];
+  dataSource!: MatTableDataSource<INote>;
+  pageSizes = [5, 10, 25, 100];
 
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
+  constructor(private notesService: NotesService, private snackBarService: MatSnackBar,
+    private router: Router) {
+    // // Create 100 users
+    // const users = Array.from({ length: 100 }, (_, k) => createNewUser(k + 1));
+
+    // // Assign the data to the data source for the table to render
+    // this.dataSource = new MatTableDataSource(users);
+  }
+
+  ngOnInit(): void {
+      this.getAllNotes();
+  }
+
+  private getAllNotes(): void {
+    this.notesService.getAllNotes().subscribe((res: INote[]) => {
+      const userId = localStorage.getItem(LOCALSTORAGE_KEYS.ID) ?? '';
+      if(!userId) {
+        this.showSnackbar('User does not exist ...');
+        localStorage.clear();
+        this.router.navigate(['login']);
+      } else {
+        const filteredData = res.filter((obj: INote) => obj.createdBy === userId);
+        this.dataSource = new MatTableDataSource(filteredData);
+        this.dataSource.paginator = this.paginator;
+        this.dataSource.sort = this.sort;
+      }
+    })
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
+  private showSnackbar(message: string, content = 'X', duration = 3000) {
+    this.snackBarService.open(message, content, {
+      direction: 'ltr',
+      duration: duration,
+      verticalPosition: 'top',
+      horizontalPosition: 'right'
+    });
+  }
 }
