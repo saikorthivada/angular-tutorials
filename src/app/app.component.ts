4b469e105fe798f823c86839768e45502a766e83
Sai Kumar <korthivadasaikumar@gmail.com>
2024-11-26 15:06:40 -0500

    V-19 main branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [ChildComponent],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
