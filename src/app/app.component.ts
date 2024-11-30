0456404a5d945b038800b5b88afe10462a8aeda0
Sai Kumar korthivada <korthivadasaikumar@gmail.com>
2024-11-30 00:50:31 -0500

    V-19 view child

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, viewChild } from '@angular/core';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [ChildComponent],
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'v-19';
+  readonly element = viewChild.required<ElementRef>("element");
+
+  readonly childElement = viewChild.required<ChildComponent>("child");
+
+  ngAfterViewInit() {
+    const element = this.element();
+
+    const childElement = this.childElement();
+    console.log(childElement.titleChild);
+    element.nativeElement.focus();
+  }
+
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.scss'
+})
+export class ChildComponent {
+  titleChild = "Techshareskk Angular 19 version";
+}
