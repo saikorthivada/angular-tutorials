e35fafc95c3eefb4be2bdf3c55d7353c8ee3efdf
Sai Kumar korthivada <korthivadasaikumar@gmail.com>
2024-11-30 01:17:15 -0500

    Model signal v19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, effect, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [FormsModule],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'v-19';
+
+  firstName = model<string>("");
+
+  studentDetails = model<{id: number, name: string}>({
+    id: 0,
+    name: "Sai kumar"
+  })
+
+  constructor() {
+    effect(() => {
+      console.log("effect", this.firstName());
+    })
+  }
+
+  updateName() {
+    // this.firstName.set(Math.random().toString());
+    this.firstName.update((value) => value + Math.random().toString());
+  }
 }
