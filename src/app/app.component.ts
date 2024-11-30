2dc088153272337e701385ff6885deefc59dc9a6
Sai Kumar korthivada <korthivadasaikumar@gmail.com>
2024-11-30 01:32:19 -0500

    hmr v-19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, model } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ChildComponent } from './child/child.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [FormsModule,ChildComponent],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'v-19';
+
+  inputElement = model();
+
+  constructor() {}
 }
---
+++ src/app/child/child.component.ts
@@ -0,0 +1,11 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  imports: [],
+  templateUrl: './child.component.html',
+  styleUrl: './child.component.scss'
+})
+export class ChildComponent {
+  value = signal("Sai kumar");
+}
