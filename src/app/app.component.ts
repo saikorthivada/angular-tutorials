49ad870136ee2dc42fc91b33cd687d700ccf49ce
Sai Kumar korthivada <korthivadasaikumar@gmail.com>
2024-12-01 15:23:35 -0500

    Resource signals

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, computed, resource, signal } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  imports: [CommonModule],
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'v-19';
+
+  id = signal(1);
+
+  todoResource = resource({
+    request: () => ({id: this.id()}),
+    loader: ({request}) => fetch(`https://jsonplaceholder.typicode.com/todos/${request.id}`).then((res) => res.json())
+  });
+
+  todo = computed(() => JSON.stringify(this.todoResource.value()));
+
+  next() {
+    this.id.update((value) => value + 1);
+  }
+  // todosResource = resource({
+  //   loader: () => fetch(`https://jsonplaceholder.typicode.com/todos`).then((res) => res.json())
+  // });
+
+  // list = computed(() => this.todosResource.value());
+
+  reload() {
+    this.todoResource.reload();
+  }
 }
