fa82e81bb60c09de16e8785e0b18d5dab5fddb05
Sai Kumar korthivada <korthivadasaikumar@gmail.com>
2025-03-15 11:28:12 -0400

    Hackerrank List manager in angular 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, model, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [FormsModule]
 })
 export class AppComponent {
   title = 'v-19';
+  modalValue = model("");
+  list = signal<string[]>([]);
+  addItem() {
+    if (this.modalValue()) {
+      this.list.update((value) => [...value, this.modalValue()]);
+      this.modalValue.set("");
+    }
+  }
 }
